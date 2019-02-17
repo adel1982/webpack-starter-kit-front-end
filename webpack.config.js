bca637359078e8e6f02542b05906fe742302d5c3
Adel Kamel <adelkamel1982@gmail.com>
2019-02-17 11:06:13 +0100

    Adding Jarvis dashboard

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,23 @@
 const path                 = require('path');
 const HtmlWebpackPlugin    = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Jarvis               = require("webpack-jarvis");
 
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-	template:'./src/index.html'
+	template: './src/index.html'
 })
 
 module.exports = {
-	entry: './src/js/index.js',
+	entry : './src/js/index.js',
 	output: {
-		path: path.join(__dirname, '/build/'), 
+		path    : path.join(__dirname, '/build/'),
 		filename: 'main.js'
 	},
 	module: {
 		rules: [
 			{
 				test: /\.(js|jsx)$/,
-				exclude: /node_module/,
+				exclude: /node_modules/,
 				use: {loader: 'babel-loader'}
 			},
 			{
@@ -44,6 +45,9 @@ module.exports = {
 	},
 	plugins: [
 		htmlWebpackPlugin,
-		new MiniCssExtractPlugin()
+		new MiniCssExtractPlugin(),
+		new Jarvis({
+			port: 1337 
+		})
 	]
 }
