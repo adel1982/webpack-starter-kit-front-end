3a875af0142d6d4f55e0450f71503f6e3eeda6ff
Adel Kamel <adelkamel1982@gmail.com>
2019-04-08 15:02:19 +0200

    Update eslint conf

--- webpack.common.js
+++ webpack.common.js
@@ -18,6 +18,11 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader'
       },
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: ['babel-loader', 'eslint-loader']
+      // },
       {
         test: /\.ico$/,
         loader: 'file-loader',
