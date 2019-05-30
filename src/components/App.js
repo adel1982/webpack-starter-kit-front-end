44489c4e46a1e339db380239ad6c3262d865b8b2
Adel Kamel <adelkamel1982@gmail.com>
2019-05-30 15:02:25 +0200

    Homepage improvement

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
-import React, { Component } from "react";
-import sassy from "../assets/scss/main.scss";
+import React, { Component } from "react"
+import sassy from "../assets/scss/main.scss"
 
-import favicon from "../assets/img/favicon.ico";
-import webpackSchema from "../assets/img/webpack.jpg";
+import favicon from "../assets/img/favicon.ico"
+import webpackSchema from "../assets/img/schema-webpack.jpg"
+import webpackLogo from "../assets/img/logo-webpack.svg"
+import gitHubLogo from "../assets/img/logo-github.svg"
 
 class App extends Component {
   render() {
     return (
-      <div className={sassy.app}>
-        <img src={webpackSchema} alt="" />
+      <div>
+        <img src={webpackLogo} alt="" className={sassy.logo} />
+        <h1>Starter Kit</h1>
+        <a href="https://github.com/adel1982/webpack-starter-kit-front-end" target="_blank">
+          <img src={gitHubLogo} alt="" className={sassy.logoGithub}  />
+        </a>
+        <img src={webpackSchema} alt="" className={sassy.schema} />
       </div>
     );
   }
--- src/vendor.js
+++ src/vendor.js
@@ -1,1 +1,1 @@
-console.log('Hello from vendor')
+const toto = console.log('Hello from vendor')
